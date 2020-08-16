df929aaad19b0a2d62bcdc9d1e415f83ac86e6b8
Lnardon <lucaslnardon@gmail.com>
2020-08-16 17:52:27 -0300

    Finished my first usercard web component

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const userCardTemplate = document.createElement("template");
+userCardTemplate.innerHTML = `
+<style>
+    h1 {
+        color: #313131;
+        font-weight : 700;
+        text-align: center;
+        font-size: 51px;
+        font-family: 'Monospace', Arial
+    }
+</style>
+<div class="user-card">
+    <h1></h1>
+</div>
+`;
+
 class UserCard extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `${this.getAttribute("name")}`;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(userCardTemplate.content.cloneNode(true));
+    this.shadowRoot.querySelector("h1").innerText = this.getAttribute("name");
   }
 }
 
