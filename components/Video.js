f00cf2809719aaedb2229aa7bdc7e13eeff92144
lnardon <lucaslnardon@gmail.com>
2020-09-13 10:30:29 -0300

    Added player functionality and controls bar animation

--- components/Video.js
+++ components/Video.js
@@ -8,22 +8,35 @@ class Video extends HTMLElement {
     this.render();
     const video = this.shadowRoot.getElementById("video");
     const controls = this.shadowRoot.getElementById("controls");
+    const settings = this.shadowRoot.getElementById("settings");
+    const backdrop = this.shadowRoot.getElementById("videoBackdrop");
+
+    let lastTimeOut;
     this.shadowRoot.getElementById("play").addEventListener("click", () => {
-      if (video.classList.contains("paused")) {
-        video.play();
-        video.classList.remove("paused");
-        video.classList.add("playing");
-      } else {
+      if (video.classList.contains("playing")) {
         video.pause();
         video.classList.remove("playing");
         video.classList.add("paused");
+      } else {
+        video.play();
+        video.classList.remove("paused");
+        video.classList.add("playing");
       }
     });
     video.addEventListener("mouseenter", () => {
-      controls.style.display = "block";
+      clearTimeout(lastTimeOut);
+      controls.style.display = "flex";
+      backdrop.style.animation = "backdropAnimation .3s ease-out forwards";
     });
     video.addEventListener("mouseout", () => {
-      setTimeout(() => (controls.style.display = "none"), 3000);
+      lastTimeOut = setTimeout(() => {
+        controls.style.display = "none";
+        backdrop.style.animation =
+          "backdropAnimationReverse .3s ease-in forwards";
+      }, 2000);
+    });
+    settings.addEventListener("click", () => {
+      video.playbackRate = 2;
     });
   }
 
@@ -35,32 +48,82 @@ class Video extends HTMLElement {
             position: relative;
             padding:0px;
             margin:0px;
+            box-sizing: border-box;
         }
 
         #video {
-            max-width:100%;
+            width:100%;
+        }
+
+        #videoBackdrop {
+            opacity: 0;
+            position: absolute;
+            width: 100%;
+            height: calc(100% - 7px);
+            background-color: rgba(0,0,0,0.35);
+            box-sizing: border-box;
         }
 
         .controls {
             position: absolute;
+            display: flex;
+            align-items: center;
             width: 100%;
-            height: 2rem;
             background-color: #171717;
             box-sizing: border-box;
-            bottom: 0px;
+            bottom: 0rem;
+            animation: showControls 0.5s ease-in-out alternate;
+            overflow: hidden
         }
 
         button {
             background-color: transparent;
             border: none;
             outline: none;
-            cursor: pointer
+            cursor: pointer;
+        }
+
+        .iconBtn {
+            width: 20px
+        }
+
+        @keyframes showControls {
+            from {
+                height: 0rem
+            }
+            to {
+                height: 2rem;
+            }
+        }
+
+        @keyframes backdropAnimation {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+
+        @keyframes backdropAnimationReverse {
+            from {
+                opacity:1;
+            }
+            to {
+                opacity: 0;
+            }
         }
     </style>
-    <div class="container">
-        <video src="./vid.mp4" id="video"></video>
+    <div class="container" id="container">
+        <div id="videoBackdrop"></div>
+        <video src="./vid.mp4" type="video/mp4" id="video"></video>
         <div class="controls" id="controls">
-            <button id="play">=></button>
+            <button id="play">
+                <img src="https://www.iconsdb.com/icons/preview/white/video-play-3-xxl.png" class="iconBtn"/>
+            </button>
+            <button id="settings">
+                <img src="https://www.iconsdb.com/icons/preview/white/gear-xxl.png" class="iconBtn"/>
+            </button>
         </div>
     </div>
     `;
