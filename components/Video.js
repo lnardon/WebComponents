0badedc12b2e7223bd31ffb42e0a88bb903e2550
lnardon <lucaslnardon@gmail.com>
2020-09-14 07:39:26 -0300

    Added fullscreen option

--- components/Video.js
+++ components/Video.js
@@ -6,10 +6,12 @@ class Video extends HTMLElement {
 
   connectedCallback() {
     this.render();
+    const container = this.shadowRoot.getElementById("container");
     const video = this.shadowRoot.getElementById("video");
     const controls = this.shadowRoot.getElementById("controls");
     const settings = this.shadowRoot.getElementById("settings");
     const backdrop = this.shadowRoot.getElementById("videoBackdrop");
+    const fullscreenBtn = this.shadowRoot.getElementById("fullscreen");
 
     let lastTimeOut;
     this.shadowRoot.getElementById("play").addEventListener("click", () => {
@@ -29,14 +31,39 @@ class Video extends HTMLElement {
       backdrop.style.animation = "backdropAnimation .3s ease-out forwards";
     });
     video.addEventListener("mouseout", () => {
+      clearTimeout(lastTimeOut);
       lastTimeOut = setTimeout(() => {
         controls.style.display = "none";
         backdrop.style.animation =
           "backdropAnimationReverse .3s ease-in forwards";
-      }, 2000);
+      }, 1500);
+    });
+    video.addEventListener("click", () => {
+      if (video.classList.contains("playing")) {
+        video.pause();
+        video.classList.remove("playing");
+        video.classList.add("paused");
+      } else {
+        video.play();
+        video.classList.remove("paused");
+        video.classList.add("playing");
+      }
     });
     settings.addEventListener("click", () => {
       video.playbackRate = 2;
+    });
+    controls.addEventListener("mouseenter", () => {
+      clearTimeout(lastTimeOut);
+    });
+    controls.addEventListener("mouseout", () => {
+      lastTimeOut = setTimeout(() => {
+        controls.style.display = "none";
+        backdrop.style.animation =
+          "backdropAnimationReverse .3s ease-in forwards";
+      }, 1500);
+    });
+    fullscreenBtn.addEventListener("click", () => {
+      video.requestFullscreen();
     });
   }
 
@@ -69,11 +96,13 @@ class Video extends HTMLElement {
             display: flex;
             align-items: center;
             width: 100%;
-            background-color: #171717;
+            height: 3rem;
+            background-color: rgba(21,21,21,1);
             box-sizing: border-box;
-            bottom: 0rem;
+            bottom: 0.5rem;
             animation: showControls 0.5s ease-in-out alternate;
-            overflow: hidden
+            overflow: hidden;
+            padding: 0rem 1rem;
         }
 
         button {
@@ -87,12 +116,30 @@ class Video extends HTMLElement {
             width: 20px
         }
 
+        #videoTimeline {
+            width: 100%;
+            height: 5px;
+            background-color: #fafafa
+        }
+
+        #videoTimelineCompleted {
+            width: 30%;
+            height: 5px;
+            background-color: red 
+        }
+
+        #fullscreen {
+            position: absolute;
+            top: 1rem;
+            right: 1rem
+        }
+
         @keyframes showControls {
             from {
                 height: 0rem
             }
             to {
-                height: 2rem;
+                height: 3rem;
             }
         }
 
@@ -117,6 +164,9 @@ class Video extends HTMLElement {
     <div class="container" id="container">
         <div id="videoBackdrop"></div>
         <video src="./vid.mp4" type="video/mp4" id="video"></video>
+        <button id="fullscreen">
+            <img src="https://www.iconsdb.com/icons/preview/white/fit-to-width-xxl.png" class="iconBtn"/>
+        </button>
         <div class="controls" id="controls">
             <button id="play">
                 <img src="https://www.iconsdb.com/icons/preview/white/video-play-3-xxl.png" class="iconBtn"/>
